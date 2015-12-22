7d5c19ea0abc526d93a4537ac9cb56dad9962028
Flascher <alexanderflasch@gmail.com>
2015-12-22 13:07:56 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+
+var apiKey = require('./config.json').apiKey;
+var Crystalys = require('crystalys').setApiKey(apiKey);
+var crystalys = new Crystalys();
+
+var app = express();
+
+app.get('/', function(req, re) {
+    res.send('You\'ve reached a Daedalus API server. You shouldn\'t be seeing this... whoops! :^)');
+});
+
+for (var schema in crystalys) {
+    console.log(schema);
+}
+
+app.listen(8080);
